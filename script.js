bcbc133af1da5d04d6c8c1ca58c5c069fff2db3b
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-12 17:07:53 -0600

    Create Validation for grid size

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 let makeGrid = (size) => {
     const grid = document.querySelector('.grid');
-grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
-grid.style.gridTemplateRows = 'repeat(16, 1fr)';
+    let squares = grid.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-for(let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.style.backgroundColor = "blue";
-    grid.insertAdjacentElement('beforeend', square);
+    let newSize = size * size;
+    for(let i = 0; i < newSize; i++) {
+        let square = document.createElement('div');
+        square.style.backgroundColor = "blue";
+        grid.insertAdjacentElement('beforeend', square);
+    }
 }
+
+makeGrid(16)
+
+let changeSize = (input) => {
+    if (input >= 2 && input <= 100) {
+        makeGrid(input);
+    } else {
+        console.log("too many squares")
+    }
 }
